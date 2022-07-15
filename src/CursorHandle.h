dde1a7553fc60e62a4a89b465eb2c796246763c4
donarturo <arturwrona91@gmail.com>
2022-07-15 16:00:07 +0200

    Rename class CursorLimits to CursorHandle

---
+++ src/CursorHandle.h
@@ -0,0 +1,37 @@
+#ifndef CURSORHANDLE_H
+#define CURSORHANDLE_H
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <memory>
+#include "PropertiesTypes.h"
+#include "UserInterface.h"
+
+class UserInterface;
+
+class CursorHandle
+{
+        position cursorPosition;
+        position cursorPositionPrevious;
+    public:
+        CursorHandle(int width, int height);
+        CursorHandle(UserInterface *ui);
+        ~CursorHandle();
+        void setRange(Range* range, int min, int max);
+        std::string getParametersMsg();
+        bool positionIsPossible();
+        bool positionIsOnBorder();
+        bool moveIsPossible();
+        bool positionIsMoved();
+        void move(int x, int y);
+        void setCursorPosition(int x, int y);
+        position getCursorPosition();
+    protected:
+        UserInterface *ui;
+        Range positionX;
+        Range positionY;
+        
+        
+        
+};
+#endif
