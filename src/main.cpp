9c0a08ef67a8053dd3820fcb1b7526ef6c7b8914
donarturo <arturwrona91@gmail.com>
2022-07-28 20:46:58 +0200

    Add test textes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 #include "UserInterface.h"
 
+void printOption(std::string opt, bool b);
+std::string enableText(bool b);
+
 int main() 
 {
+    // test
+#ifdef _TEST_
+#  ifdef _PDCURSES_
+      printOption("PDCurses", true);
+#  endif // PD_CURSES
+#  ifdef _SDL_
+      printOption("SDL", true);
+#  endif // SDL
+printf("Print any key to continue: ");
+std::cin.get();
+#endif // test
     UserInterface interface;
     return 0;
+}
+
+
+void printOption(std::string opt, bool b)
+{
+    std::cout << "==> " << opt << ":" << enableText(b) << std::endl;
+}
+
+std::string enableText(bool b)
+{
+    return b ? " enabled " : " disabled ";
 }
